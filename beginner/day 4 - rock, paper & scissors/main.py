a050cbd7ed1668d31838810b7f6847b7b34624d1
daleale <daleale-se@gmail.com>
2024-07-07 00:41:34 -0300

    debugging day 4 exercise

--- beginner/day 4 - rock, paper & scissors/main.py
+++ beginner/day 4 - rock, paper & scissors/main.py
@@ -31,35 +31,49 @@ import random
 
 choice_images = [rock, paper, scissors]
 choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors. \n"))
-computer_choice = random.choice([0, 1, 2])
 
-if not (0 < choice < 3):
-    print("Incorrect choice")
+if 0 < choice < 3:
+    computer_choice = random.choice([0, 1, 2])
 
-print("Your chose: ")
-print(choice_images[choice])
+    print("Your chose: ")
+    print(choice_images[choice])
 
-print("Computer chose: ")
-print(choice_images[computer_choice])
+    print("Computer chose: ")
+    print(choice_images[computer_choice])
 
-if choice == 0:
-    if computer_choice == 0:
-        print("DRAW!")
-    elif computer_choice == 1:
-        print("YOU LOSE!")
-    else:
-        print("YOU WIN!")
-elif choice == 1:
-    if computer_choice == 0:
-        print("YOU WIN!")
-    elif computer_choice == 1:
-        print("DRAW!")
-    else:
-        print("YOU LOSE!")
-else: 
-    if computer_choice == 0:
-        print("YOU LOSE!")
-    elif computer_choice == 1:
-        print("YOU WIN!")
-    else:
-        print("DRAW!")
+    if choice == 0:
+        if computer_choice == 0:
+            print("DRAW!")
+        elif computer_choice == 1:
+            print("YOU LOSE!")
+        else:
+            print("YOU WIN!")
+    elif choice == 1:
+        if computer_choice == 0:
+            print("YOU WIN!")
+        elif computer_choice == 1:
+            print("DRAW!")
+        else:
+            print("YOU LOSE!")
+    else: 
+        if computer_choice == 0:
+            print("YOU LOSE!")
+        elif computer_choice == 1:
+            print("YOU WIN!")
+        else:
+            print("DRAW!")
+else:
+    print("Invalid number")
+
+
+# another way:
+# if user_choice == 0 and computer_choice == 2:
+#     print("You win!")
+# elif computer_choice == 0 and user_choice == 2:
+#     print("You lose")
+# elif computer_choice > user_choice:
+#     print("You lose")
+# elif user_choice > computer_choice:
+#     print("You win!")
+# elif computer_choice == user_choice:
+#     print("It's a draw")
