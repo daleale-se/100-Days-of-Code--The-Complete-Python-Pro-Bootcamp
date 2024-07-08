12fe0e37588f4906b64a0c49ef7d0f624bd941eb
daleale <daleale.se@gmail.com>
2024-07-07 23:36:47 -0300

    Completing day 6 exercise

---
+++ beginner/day 6 - Escaping the Maze/main.py
@@ -0,0 +1,25 @@
+# Reeborg's World
+# Pass all the three tests
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+    
+# def turn_around():
+#     turn_left()
+#     turn_left()
+
+# while front_is_clear():
+#     move()
+   
+# turn_around()
+    
+# while not at_goal():
+#     if right_is_clear():
+#         turn_right()
+#         move()
+#     elif front_is_clear():
+#         move()
+#     else:
+#         turn_left()
