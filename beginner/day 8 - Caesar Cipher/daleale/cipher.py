7f7d1ad890570a10f508ae14237cd0debc5c6ac6
daleale <daleale.se@gmail.com>
2024-07-09 20:11:34 -0300

    Creating my own Caesar Cipher (day 8)

---
+++ beginner/day 8 - Caesar Cipher/daleale/cipher.py
@@ -0,0 +1,36 @@
+
+def shifting(message: str, shift:int):
+    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    message_list = list(message)
+    for i in range(len(message_list)):
+        if message_list[i] in alphabet:
+            index = alphabet.index(message_list[i]) + shift
+            if index >= len(alphabet) - 1:
+                index -= len(alphabet)
+            message_list[i] = alphabet[index]
+    return "".join(message_list)
+
+def cipher():
+    
+    start = "yes"
+    
+    while start == "yes":
+        option = input("Type 'encode' to encrypt, type 'decode' to decrypt: \n")
+
+        while option not in ["encode", "decode"]:
+            option = input("Type 'encode' to encrypt, type 'decode' to decrypt: \n")
+
+        if option == 'encode':
+            message = input("Type your message: \n")
+            shift = int(input("Type the shift number: \n"))
+            result = shifting(message, shift)
+            print(f"Here's the encoded result: {result}")
+        else: 
+            message = input("Type your message: \n")
+            shift = int(input("Type the shift number: \n"))
+            result = shifting(message, -shift)
+            print(f"Here's the encoded result: {result}")
+        
+        start = input("Type 'yes' if you want to go again. Otherise type 'no'. \n")
+        
+    print("Goodbye")
---
+++ beginner/day 8 - Caesar Cipher/main.py
@@ -0,0 +1,12 @@
+import daleale.cipher as daleale
+import angela.cipher as angela
+
+def main():
+    
+    # daleale.cipher()
+    
+    angela.cipher()
+    
+    return 0
+
+main()
