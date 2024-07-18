4366dbd0276a008a68a99a12611563f9ff41a717
daleale <daleale.se@gmail.com>
2024-07-17 22:31:44 -0300

    Testing pandas (day 25)

---
+++ Intermediate/day 25 - Pandas library/main.py
@@ -0,0 +1,50 @@
+# import csv
+
+# def main():
+#     with open("./weather_data.csv") as file:
+#         data = csv.reader(file)
+#         temperatures = []
+#         for row in data:
+#             print(row)
+#             if row[1] != "temp":
+#                 temperatures.append(int(row[1]))
+#         print(temperatures)
+
+import pandas
+
+
+def main():
+
+    # data = pandas.read_csv("./weather_data.csv")
+
+    # print(data)             # Dataframe
+    # print(data["temp"])     # Series
+    #
+    # data_dict = data.to_dict()
+    # print(data_dict)
+    #
+    # data_list = data["temp"].to_list()
+    # print(data_list)
+    #
+    # print(data["temp"].mean())
+    # print(data["temp"].max())
+    #
+    # print(data.condition.to_list())
+    # print(data.day.to_list())
+    #
+    # print(data[data.day == "Sunday"])
+    # print(data[data.temp == data.temp.max()])
+
+    # print(data[data.day == "Monday"].temp)
+    # print(data.temp[0] * (9 / 5) + 32)
+
+    data_dict = {
+        "students": ["Amy", "James", "Angela"],
+        "scores": [76, 56, 65]
+    }
+
+    data_d = pandas.DataFrame(data_dict)
+    print(data_d)
+
+
+main()
