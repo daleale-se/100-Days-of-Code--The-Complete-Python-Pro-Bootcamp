27d29da4b9a67ca2c3c0343c69d1b5d5ec93265d
daleale <daleale.se@gmail.com>
2024-08-07 13:29:10 -0300

    Coding day 39 & 40 project (Flight Search)

--- Intermediate+/day 39 & 40 - Flight Search/data_manager.py
+++ Intermediate+/day 39 & 40 - Flight Search/data_manager.py
@@ -1,3 +1,27 @@
+import os
+import requests
+from dotenv import load_dotenv
+from flight_search import FlightSearch
+
+load_dotenv()
+
+# This class is responsible for talking to the Google Sheet.
+
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    pass
+    def __init__(self):
+        self.destinations = None
+        self.do_fetch()
+
+    def do_fetch(self):
+
+        sheety_api = "https://api.sheety.co/f2cc9daa00cc57ef59fe38c1ed2ebd85/flightDeals/prices"
+        sheety_headers = {
+            "Authorization": os.getenv("SHEETY_TOKEN")
+        }
+
+        res = requests.get(url=sheety_api, headers=sheety_headers)
+        self.destinations = res.json()["prices"]
+
+    def do_search(self, flight_search: FlightSearch):
+        for destiny in self.destinations:
+            flight_search.cheapest_flights(destiny)
--- Intermediate+/day 39 & 40 - Flight Search/flight_search.py
+++ Intermediate+/day 39 & 40 - Flight Search/flight_search.py
@@ -1,3 +1,34 @@
+import os
+import requests
+from dotenv import load_dotenv
+from datetime import *
+
+load_dotenv()
+
+# This class is responsible for talking to the Flight Search API.
 class FlightSearch:
-    #This class is responsible for talking to the Flight Search API.
-    pass
+    def __init__(self):
+        self.flights = None
+
+    def cheapest_flights(self, destination):
+
+        ORIGIN = "MAD"
+
+        date_now = datetime.now()
+        from_day = date_now.strftime("%Y-%m-%d")
+        to_day = date_now + timedelta(days=180)
+        to_day = to_day.strftime("%Y-%m-%d")
+
+        amadeus_api = "https://test.api.amadeus.com/v1/shopping/flight-dates"
+        amadeus_headers = {
+            "Authorization": os.getenv("AMADEUS_TOKEN")
+        }
+        amadeus_parameters = {
+            "origin": ORIGIN,
+            "destination": destination["iataCode"],
+            # "departureDate": f"{from_day},{to_day}"
+        }
+
+        print(destination["city"], destination["iataCode"], destination["lowestPrice"])
+        res = requests.get(url=amadeus_api, headers=amadeus_headers, params=amadeus_parameters)
+        print(res.json())
